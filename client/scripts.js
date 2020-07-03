50ce719f7a24af8a34bdd9a91148e055d20879df
Mel <melissakheying@gmai.com>
2020-07-03 16:39:33 -0500

    Select form with querySelector and listen for Submit

--- client/scripts.js
+++ client/scripts.js
@@ -1,6 +1,12 @@
-document.querySelector('form').addEventListener('submit', function(event) {
-event.preventDefault()
+console.log(document.querySelector('.screen-reader-text'))
+console.log(document.querySelectorAll('input'))
 
-console.log('YOOOOOOO')
+document.querySelector('form').addEventListener('submit', function (event) {
+  event.preventDefault() // we do not want default browser behavior
 
+  console.log('YOOO')
 })
+
+const x = 3
+// gives back undefined primitive data type
+console.log(x.happy)
