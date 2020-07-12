94d1dcb01a52d86517f67352f6eb501ec6514d31
Mel <melissakheying@gmai.com>
2020-07-12 18:45:09 -0500

    Fix statment in alreadyAUser

--- client/scripts.js
+++ client/scripts.js
@@ -7,20 +7,18 @@ document.querySelector('form').addEventListener('submit', function (event) {
   event.preventDefault() // we do not want default browser behavior
 
   // add new users to array
-  const add2Users = {
+  const add2Users = {}
 
-    firstName: document.getElementById('firstName').value,
-    lastName: document.getElementById('lastName').value,
-    email: document.getElementById('email').value,
-    phone: document.getElementById('phone').value,
-    userName: document.getElementById('userName').value,
-    password: document.getElementById('password').value
-
-  }
+  add2Users.firstName = document.getElementById('firstName').value
+  add2Users.lastName = document.getElementById('lastName').value
+  add2Users.email = document.getElementById('email').value
+  add2Users.phone = document.getElementById('phone').value
+  add2Users.userName = document.getElementById('userName').value
+  add2Users.password = document.getElementById('password').value
 
   // check to see if user already exists
   const alreadyAUser = users.find(function (user) {
-    return user.userName === add2Users.userName
+    return user.userName === add2Users.userName && user.password === add2Users.password
   })
 
   // Add if userName does not exist
